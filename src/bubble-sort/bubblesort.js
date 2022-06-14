f4a3b7bbb528ed1c273952f9a8f867492c22674d
Shivshankar Shejwal <shiv.shejwal@thoughtworks.com>
2022-06-14 17:23:05 +0530

    optimisation added for checking if anything swapped

--- src/bubble-sort/bubblesort.js
+++ src/bubble-sort/bubblesort.js
@@ -1,14 +1,18 @@
 export const bubbleSort = (arr) => {
   for (let iteratorOuter = 0; iteratorOuter < arr.length; iteratorOuter++) {
-    for (
-      let iteratorInner = 0;
-      iteratorInner < arr.length - 1;
-      iteratorInner++
-    ) {
-      if (arr[iteratorInner] > arr[iteratorInner + 1]) {
-        const temp = arr[iteratorInner];
-        arr[iteratorInner] = arr[iteratorInner + 1];
-        arr[iteratorInner + 1] = temp;
+    let anythingSwapped = false;
+    if (!anythingSwapped) {
+      for (
+        let iteratorInner = 0;
+        iteratorInner < arr.length - 1;
+        iteratorInner++
+      ) {
+        if (arr[iteratorInner] > arr[iteratorInner + 1]) {
+          anythingSwapped = true;
+          const temp = arr[iteratorInner];
+          arr[iteratorInner] = arr[iteratorInner + 1];
+          arr[iteratorInner + 1] = temp;
+        }
       }
     }
   }
